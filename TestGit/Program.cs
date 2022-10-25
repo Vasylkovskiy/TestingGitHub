7765c6e99a0806456de76c54b657d3aa43e6974c
Vladyslav Vasylkivskyi <v.vasylkovskiy@gmail.com>
2022-10-25 19:38:20 +0300

    Test1

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -7,6 +7,7 @@ namespace TestGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("25-10-2022");
         }
     }
 }
